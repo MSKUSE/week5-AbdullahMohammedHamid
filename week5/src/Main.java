d87366f1a8a189cb098b730459f5c3d9bb69cfbf
Abdullah Mohammed <abboud.qt2004@gmail.com>
2025-03-20 13:18:41 +0300

    projet

--- week5/src/Main.java
+++ week5/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Point p1 = new Point();
+        String s1 = new String("Abdullah");
+        System.out.println(p1.xCoord);
+        System.out.println(p1.yCoord);
+
+        Point p2 = new Point( 3,  4);
+        System.out.println(p2.xCoord);
+        System.out.println(p2.yCoord);
+
+        Point p3 = new Point( 5);
+        System.out.println(p3.xCoord);
+        System.out.println(p3.yCoord);
+
+        Point tLeft = new Point(3,4);
+        Rectangle r1 = new Rectangle(
+                tLeft,
+                 3,
+                4
+        );
+        System.out.println(r1.getsideA());
+        System.out.println(r1.get.SideB());
+        System.out.println(r1.get.Topleft().xCoord);
+        System.out.println(r1.get.Topleft().yCoord);
+
+
     }
 }
